6e288070f45a6f49729e7d298d2857cbae59ab53
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-02-26 16:29:08 +0530

    Finished REST api for login route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -25,7 +25,24 @@ router.post("/register", async (req, res) => {
     }
 });
 
-// get all users 
+// login
+
+router.post("/login", async (req, res) => {
+    try {
+        // find user
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("Wrong username or password!");
+
+        // validate user
+        const validate = await bcrypt.compare(req.body.password, user.password);
+        !validate && res.status(400).json("Wrong username or password!");
+
+        // send response
+        res.status(200).json({_id: user._id, username: user.username});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 

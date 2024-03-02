818a8cc4eba63b23a2f8ceb6141fcb95e056a0df
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-03-02 14:53:36 +0530

    Created popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,40 @@
 //import { useState } from 'react'
+import './app.css';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import StarIcon from '@mui/icons-material/Star';
 
 function App() {
-
-  return (<></>)
-  
+  return (
+    <MapContainer center={[48.8584, 2.2945]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[48.8584, 2.2945]}>
+        <Popup>
+          {/* A pretty CSS3 popup. <br /> Easily customizable. */}
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">Eiffel Tower</h4>
+            <label>Review</label>
+            <p className='desc'>Beautiful place. I like it</p>
+            <label>Rating</label>
+            <div className="stars">
+              <StarIcon className='star'/>
+              <StarIcon className='star'/>
+              <StarIcon className='star'/>
+              <StarIcon className='star'/>
+              <StarIcon className='star'/>
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>xyz</b></span>
+            <span className="date">1 hour ago</span>
+          </div>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
-export default App
+export default App;

527924df49e6f96f2385b07b93f739040e3bca3c
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-03-06 22:56:26 +0530

    Modified popups

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
-//import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './app.css';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import StarIcon from '@mui/icons-material/Star';
+import axios from 'axios';
+import {format} from 'timeago.js';
 
 function App() {
+
+  const [pins, setPins] = useState([]);
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get('/pins');
+        setPins(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPins();
+  }, []);
+
+
   return (
-    <MapContainer center={[48.8584, 2.2945]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[20.5937, 78.9629]} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[48.8584, 2.2945]}>
+
+      {pins.map(p => (
+<>
+      <Marker position={[p.lat, p.long]}>
         <Popup>
-          {/* A pretty CSS3 popup. <br /> Easily customizable. */}
           <div className="card">
             <label>Place</label>
-            <h4 className="place">Eiffel Tower</h4>
+            <h4 className="place">{p.title}</h4>
             <label>Review</label>
-            <p className='desc'>Beautiful place. I like it</p>
+            <p className='desc'>{p.desc}</p>
             <label>Rating</label>
             <div className="stars">
               <StarIcon className='star'/>
@@ -28,11 +48,13 @@ function App() {
               <StarIcon className='star'/>
             </div>
             <label>Information</label>
-            <span className="username">Created by <b>xyz</b></span>
-            <span className="date">1 hour ago</span>
+            <span className="username">Created by <b>{p.username}</b></span>
+            <span className="date">{format(p.createdAt)}</span>
           </div>
         </Popup>
       </Marker>
+</>
+      ))}
     </MapContainer>
   );
 }
